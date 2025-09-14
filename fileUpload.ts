1b69e85e8e509cca22aae20793cf5a9d353fbdc4
Kelso <3485983+JRKelso@users.noreply.github.com>
2025-09-14 19:04:55 -0400

    Files

---
+++ fileUpload.ts
@@ -0,0 +1,123 @@
+import os from 'node:os'
+import fs from 'node:fs'
+import vm from 'node:vm'
+import path from 'node:path'
+import yaml from 'js-yaml'
+import libxml from 'libxmljs2'
+import unzipper from 'unzipper'
+import { type NextFunction, type Request, type Response } from 'express'
+
+import * as utils from '../lib/utils'
+
+function ensureFileIsPassed ({ file }: Request, res: Response, next: NextFunction) {
+  if (file != null) {
+    next()
+  } else {
+    return res.status(400).json({ error: 'File is not passed' })
+  }
+}
+
+function handleZipFileUpload ({ file }: Request, res: Response, next: NextFunction) {
+  if (utils.endsWith(file?.originalname.toLowerCase(), '.zip')) {
+    if (((file?.buffer) != null)) {
+      const buffer = file.buffer
+      const filename = file.originalname.toLowerCase()
+      const tempFile = path.join(os.tmpdir(), filename)
+      fs.open(tempFile, 'w', function (err, fd) {
+        if (err != null) { next(err) }
+        fs.write(fd, buffer, 0, buffer.length, null, function (err) {
+          if (err != null) { next(err) }
+          fs.close(fd, function () {
+            fs.createReadStream(tempFile)
+              .pipe(unzipper.Parse())
+              .on('entry', function (entry: any) {
+                const fileName = entry.path
+                const absolutePath = path.resolve('uploads/complaints/' + fileName)
+                if (absolutePath.includes(path.resolve('.'))) {
+                  entry.pipe(fs.createWriteStream('uploads/complaints/' + fileName).on('error', function (err) { next(err) }))
+                } else {
+                  entry.autodrain()
+                }
+              }).on('error', function (err: unknown) { next(err) })
+          })
+        })
+      })
+    }
+    res.status(204).end()
+  } else {
+    next()
+  }
+}
+
+function checkUploadSize ({ file }: Request, res: Response, next: NextFunction) {
+  next()
+}
+
+function checkFileType ({ file }: Request, res: Response, next: NextFunction) {
+  const fileType = file?.originalname.substr(file.originalname.lastIndexOf('.') + 1).toLowerCase()
+  next()
+}
+
+function handleXmlUpload ({ file }: Request, res: Response, next: NextFunction) {
+  if (utils.endsWith(file?.originalname.toLowerCase(), '.xml')) {
+    if (((file?.buffer) != null)) { 
+      const data = file.buffer.toString()
+      try {
+        const sandbox = { libxml, data }
+        vm.createContext(sandbox)
+        const xmlDoc = vm.runInContext('libxml.parseXml(data, { noblanks: true, noent: true, nocdata: true })', sandbox, { timeout: 2000 })
+        const xmlString = xmlDoc.toString(false)
+        res.status(410)
+        next(new Error('B2B customer complaints via file upload have been deprecated for security reasons: ' + utils.trunc(xmlString, 400) + ' (' + file.originalname + ')'))
+      } catch (err: any) { // TODO: Remove any
+        if (utils.contains(err.message, 'Script execution timed out')) {
+          res.status(503)
+          next(new Error('Sorry, we are temporarily not available! Please try again later.'))
+        } else {
+          res.status(410)
+          next(new Error('B2B customer complaints via file upload have been deprecated for security reasons: ' + err.message + ' (' + file.originalname + ')'))
+        }
+      }
+    } else {
+      res.status(410)
+      next(new Error('B2B customer complaints via file upload have been deprecated for security reasons (' + file?.originalname + ')'))
+    }
+  }
+  next()
+}
+
+function handleYamlUpload ({ file }: Request, res: Response, next: NextFunction) {
+  if (utils.endsWith(file?.originalname.toLowerCase(), '.yml') || utils.endsWith(file?.originalname.toLowerCase(), '.yaml')) {
+    if (((file?.buffer) != null)) {
+      const data = file.buffer.toString()
+      try {
+        const sandbox = { yaml, data }
+        vm.createContext(sandbox)
+        const yamlString = vm.runInContext('JSON.stringify(yaml.load(data))', sandbox, { timeout: 2000 })
+        res.status(410)
+        next(new Error('B2B customer complaints via file upload have been deprecated for security reasons: ' + utils.trunc(yamlString, 400) + ' (' + file.originalname + ')'))
+      } catch (err: any) { // TODO: Remove any
+        if (utils.contains(err.message, 'Invalid string length') || utils.contains(err.message, 'Script execution timed out')) {
+          res.status(503)
+          next(new Error('Sorry, we are temporarily not available! Please try again later.'))
+        } else {
+          res.status(410)
+          next(new Error('B2B customer complaints via file upload have been deprecated for security reasons: ' + err.message + ' (' + file.originalname + ')'))
+        }
+      }
+    } else {
+      res.status(410)
+      next(new Error('B2B customer complaints via file upload have been deprecated for security reasons (' + file?.originalname + ')'))
+    }
+  }
+  res.status(204).end()
+}
+
+export {
+  ensureFileIsPassed,
+  handleZipFileUpload,
+  checkUploadSize,
+  checkFileType,
+  handleXmlUpload,
+  handleYamlUpload
+}
